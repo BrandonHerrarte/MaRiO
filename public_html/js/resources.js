3e8f1dc76da4a99017a7d76a3e5d0d94f13a50e9
BrandonHerrartte <almostbrandon03@gmail.com>
2014-12-15 15:02:41 -0800

    final

---
+++ public_html/js/resources.js
@@ -0,0 +1,33 @@
+
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+        {name: "Background-tiles", type:"image", src: "data/img/background-tiles.png"},
+        {name: "Meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+        {name: "mario", type:"image", src: "data/img/player1.png"},
+        {name: "cloud-tiles", type:"image", src: "data/img/cloud-tiles.png"},
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+        {name: "Level01", type: "tmx", src: "data/map/TestLVL.tmx"}
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
